4a80f104621ac91aab2c5519c7e33549fcedcbc6
rrenn45 <rrenn45@gmail.com>
2024-09-22 22:08:44 -0700

    updated to use server actions

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText, convertToCoreMessages } from 'ai';
+import { streamText, convertToCoreMessages, StreamData } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -7,10 +7,16 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const data = new StreamData();
+  data.append({test:'value'});
+
   const result = await streamText({
     model: openai('gpt-4-turbo'),
     messages: convertToCoreMessages(messages),
+    onFinish(){
+        data.close();
+    }
   });
 
-  return result.toDataStreamResponse();
+  return result.toDataStreamResponse({data});
 }
